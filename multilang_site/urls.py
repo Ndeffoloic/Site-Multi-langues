b9b4f04e066ee38fb3e40d8856ad1157b2c1d9c9
nembotnd1u <nembotnd1u@etu.univ-lorraine.fr>
2024-06-30 17:01:48 +0200

    je supprime la deuxième barre qui permet de changer la langue

--- multilang_site/urls.py
+++ multilang_site/urls.py
@@ -5,10 +5,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
+from django.views.i18n import set_language
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('set_language/', set_language, name='set_language'),
     
 ]+ i18n_patterns(
     # URLs localisées ...
