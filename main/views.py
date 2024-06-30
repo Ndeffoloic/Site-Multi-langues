09a9388f171d45b054a87a7131baa477e59fa939
nembotnd1u <nembotnd1u@etu.univ-lorraine.fr>
2024-06-30 07:20:35 +0200

    ajout des fichiers .Po et .mo

--- main/views.py
+++ main/views.py
@@ -5,7 +5,7 @@ from .models import BlogPost
 
 
 # Create your views here.
-def article_list(request):
+def article_list(request, *args, **kwargs):
     # Utilisez BlogPost.objects.all() pour récupérer tous les objets BlogPost
     articles = BlogPost.objects.all()
     return render(request, 'article_list.html', {'articles': articles})
--- multilang_site/settings.py
+++ multilang_site/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = BASE_DIR / 'templates'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'main',
 ]
 
 # Ajouter 'django.middleware.locale.LocaleMiddleware' dans MIDDLEWARE après 'django.contrib.sessions.middleware.SessionMiddleware'
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'multilang_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
