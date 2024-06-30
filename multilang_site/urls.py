195a016e3fd2db27a6758b98e8780be19b0dada9
nembotnd1u <nembotnd1u@etu.univ-lorraine.fr>
2024-06-30 12:49:46 +0200

    config pour déploiement

--- multilang_site/urls.py
+++ multilang_site/urls.py
@@ -1,6 +1,9 @@
 
+from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
 urlpatterns = [
@@ -11,3 +14,7 @@ urlpatterns = [
     # URLs localisées ...
     path('i18n/', include('django.conf.urls.i18n')),
 )
+
+urlpatterns += static(settings.MEDIA_URLS, document_root = settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
